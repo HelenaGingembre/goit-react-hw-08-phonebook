0d1cff806e867ed6c4b5a9fdf8d64e9847c98452
Helena Gingembre <e.izotovagingembre@outlook.com>
2022-12-08 10:45:31 +0100

    update style

--- src/components/AppFooter/AppFooter.styled.js
+++ src/components/AppFooter/AppFooter.styled.js
@@ -6,7 +6,6 @@ export const FooterApp = styled.footer`
   align-content: center;
   width: 100%;
   bottom: 0px;
-  //    background-color:
   background-image: linear-gradient(
     rgba(93, 196, 255, 0.5),
     rgba(255, 250, 102, 0.5)
